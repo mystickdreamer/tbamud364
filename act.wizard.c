c61f81c6b202b4e22970056c2f5b0f975d5fb76d
Brian <Brian@BrianAlienWare>
2014-11-08 18:06:54 -0600

    Admin flags

--- act.wizard.c
+++ act.wizard.c
@@ -1445,7 +1445,7 @@ ACMD(do_advance) {
         return;
     }
 
-    if (GET_ADMLEVEL > ADMLVL_GOD) {
+    if (GET_ADMLEVEL < ADMLVL_GOD) {
         send_to_char(ch, "Maybe that's not such a great idea.\r\n");
         return;
     }
